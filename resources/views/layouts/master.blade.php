bcf17890ef57bea0f89113f71ca0b314acbf6289
Artūras Černauskas <arturas@itpc.lt>
2017-07-10 21:24:51 +0300

    Setup & Introduction

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <title>@yield('title')</title>
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
